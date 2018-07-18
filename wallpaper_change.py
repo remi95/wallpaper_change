74017419f30af0a5a4fc8efb78dc20ec9d689d60
remi95 <remimafat@gmail.com>
2018-07-18 19:04:39 +0200

    add project

---
+++ wallpaper_change.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3.6
+
+# Author: Rémi Mafat.
+# This script is running by crontab.
+# It randomly takes picture on images folder and put it on desktop wallpaper.
+
+import os, random
+
+imagesFolder = "/home/remi/Images/Wallpapers/"
+images = os.listdir(imagesFolder)
+
+def getRandomImage():
+    randomImage = random.choice(images)
+    return imagesFolder + randomImage
+
+dbus_session_bus_address = 'PID=$(pgrep gnome-session | tail -n1) && export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ|cut -d= -f2-) && '
+command = dbus_session_bus_address + 'gsettings set org.gnome.desktop.background picture-uri ' + getRandomImage()
+
+os.system(command)
